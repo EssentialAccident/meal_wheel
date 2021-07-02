7b32a43dc13b3fb8dcc6919bdbd27553c92b0c6f
Lino Oropeza <linooropeza@gmail.com>
2021-07-02 17:04:15 -0400

    Created hidden field for week creation

--- app/models/week.rb
+++ app/models/week.rb
@@ -4,4 +4,13 @@ class Week < ApplicationRecord
 
   # Validations
   validates :start_date, presence: true
+
+  def create_random_days
+    days = []
+    meals = Meal.all
+    7.times do |n|
+      days.append({ date: start_date + n, week_id: id, meal_id: meals.sample.id })
+    end
+    Day.create(days)
+  end
 end
